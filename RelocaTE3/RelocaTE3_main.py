2c41a4028f56ec8253c245f68a71a296f57f9e9f
Jason Stajich <jasonstajich.phd@gmail.com>
2023-01-30 16:57:53 -0800

    add main template

---
+++ RelocaTE3/RelocaTE3_main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""RelocaTE3 Interface and CLI logic."""
+
+import argparse
+import sys
+
+
+def main():
+    """Primary RelocaTE3 CLI logic."""
+    parser = argparse.ArgumentParser(description='Run AoC Day 1')
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'),
+                        default=sys.stdin,
+                        help='Input file (or by stdin)')
